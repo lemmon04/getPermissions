7e5c4c64087a878d14fd16a6d0c6ec90c55c7547
Michael Lemmon <lemmon04@yahoo.com>
2018-06-25 11:57:43 -0700

    Added Excel Output
    Script will output an excel spreadsheet with user group privileges for each feature class.

--- getPermissions.py
+++ getPermissions.py
@@ -1,53 +1,147 @@
-import arcpy, pyodbc, os
+import arcpy, pyodbc, os, sys, glob, xlwt
+global table_Set
+global group_Set
+global DomainUsers_rights
+global GIS_Team_rights
+global GIS_LRS_editor_rights
+global GIS_Location_Rheditor_rights
+global GIS_RWD_Rheditor_rights
+global GIS_RWS_Rheditor_rights
+global GIS_TRAFINFO_Rheditor_rights
+global GIS_TRAFSAFETY_Rheditor_rights
+global Admin_RCE_rights
 
-sde_conn ="connection to geodatabase"
+#########################################################################################
+sde_conn ="C:/test.sde"
+
 arcpy.env.workspace = sde_conn
 fcList = arcpy.ListFeatureClasses()
-
-
+table_Set = set([])
+DomainUsers_rights = []
+GIS_Team_rights = []
+GIS_LRS_editor_rights = []
+GIS_Location_Rheditor_rights = []
+GIS_RWD_Rheditor_rights = []
+GIS_RWS_Rheditor_rights = []
+GIS_TRAFINFO_Rheditor_rights = []
+GIS_TRAFSAFETY_Rheditor_rights = []
+Admin_RCE_rights = []
 def GetPrivileges():
     cnxn = pyodbc.connect(
         r'DRIVER={ODBC Driver 13 for SQL Server}'
-        r';SERVER=name of server'
-        r';DATABASE=name of db'
+        r';SERVER='
+        r';DATABASE='
         r';Trusted_connection=yes'
         )
     command = "EXEC sp_table_privileges @table_name = " + table_name 
     cursor = cnxn.cursor()
     cursor.execute(command)
     result = cursor.fetchall()
-    Domain_Users = {"table": set(), "group": set(), "rights": set()}
-    GIS_Team = {"table": set(), "group": set(), "rights": set()}
-    stuff = []
+    #List of User Group names
+    Users_List = [""]
+    #List of User Group rights
+    Rights_List = []
+    i = 0
+    for x in Users_List:
+        for row in result:
+            if row[4] == x:
+                if row[5] in ("UPDATE", "INSERT", "DELETE"):
+                    table_Set.add(row[2])
+                    Rights_List[i].append("editor")
+                    break
+                elif row[5] == "SELECT":
+                    table_Set.add(row[2])
+                    Rights_List[i].append("viewer")
+            elif row == result[-1]:
+                Rights_List[i].append("No data")
+            else:
+                continue
+        i = i + 1
+
     for row in result:
-        if row[4] == "NVDOT\Domain Users":
-            if row[5] == "SELECT":
-                Domain_Users["table"].add(row[2])
-                Domain_Users["group"].add(row[4])
-                Domain_Users["rights"].add("viewer")
-
-            elif row[5] in ("UPDATE", "INSERT", "DELETE"):
-                Domain_Users["table"].add(row[2])
-                Domain_Users["group"].add(row[4])
-                Domain_Users["rights"].add("editor")
-
-        elif row[4] == "NVDOT\GIS Team":
-            if row[5] == "SELECT":
-                GIS_Team["table"].add(row[2])
-                GIS_Team["group"].add(row[4])
-                GIS_Team["rights"].add("viewer")
-
-            elif row[5] in ("UPDATE", "INSERT", "DELETE"):
-                GIS_Team["table"].add(row[2])
-                GIS_Team["group"].add(row[4])
-                GIS_Team["rights"].add("editor")
-
-    print list(Domain_Users["table"]) + list(Domain_Users["group"]) + list(Domain_Users["rights"])
-    print list(GIS_Team["table"]) + list(GIS_Team["group"]) + list(GIS_Team["rights"])
-    print "\n"
-
+        if row[4] == "NYDOT\Domain Users":
+            if row[5] in ("UPDATE", "INSERT", "DELETE"):
+                table_Set.add(row[2])
+                DomainUsers_rights.append("editor")
+                break
+            elif row[5] == "SELECT":
+                table_Set.add(row[2])
+                DomainUsers_rights.append("viewer")
+        else:
+            continue
+    
+        
 for fc in fcList:
     split = fc.split(".")
     table_name = split[2]
     GetPrivileges()
+
+    
+#Create excel spreadsheet    
+book = xlwt.Workbook()
+sheet_Permissions = book.add_sheet("Permissions")
+style_string = "font: bold on"
+bold = xlwt.easyxf(style_string)
+#Set column width
+sheet_Permissions.col(0).width  = 8000
+sheet_Permissions.col(1).width  = 6000
+sheet_Permissions.col(2).width  = 6000
+sheet_Permissions.col(3).width  = 6000
+sheet_Permissions.col(4).width  = 6000
+sheet_Permissions.col(5).width  = 6000
+sheet_Permissions.col(6).width  = 6000
+sheet_Permissions.col(7).width  = 6000
+sheet_Permissions.col(8).width  = 6000
+sheet_Permissions.col(9).width  = 6000
+#Set first row height
+sheet_Permissions.row(0).height_mismatch = True
+sheet_Permissions.row(0).height = 500
+#Set column header names in bold, wrap text
+styles = xlwt.XFStyle()
+styles.alignment.wrap = 1
+font = xlwt.Font()
+font.bold = True
+styles.font = font
+#Set each header with name of AD user groups
+sheet_Permissions.write(0,0,'Table', styles)
+sheet_Permissions.write(0,1,'NVDOT\', styles)
+sheet_Permissions.write(0,2,'NVDOT\', styles)
+sheet_Permissions.write(0,3,'NVDOT\, styles)
+sheet_Permissions.write(0,4,'NVDOT\', styles)
+sheet_Permissions.write(0,5,'NVDOT\', styles)
+sheet_Permissions.write(0,6,'NVDOT\', styles)
+sheet_Permissions.write(0,7,'NVDOT\', styles)
+sheet_Permissions.write(0,8,'NVDOT\', styles)
+sheet_Permissions.write(0,9,'NVDOT\', styles)
+#Freeze top cell
+sheet_Permissions.set_panes_frozen(True)
+sheet_Permissions.set_horz_split_pos(1)
+sheet_Permissions.set_vert_split_pos(1)
+#start from first cell below header
+row_Table = 1
+col_Table = 0
+table_List = list(table_Set)
+table_List.sort()
+#Add Tables to first column
+for x in table_List:
+    sheet_Permissions.write(row_Table, col_Table, x)
+    row_Table = row_Table + 1
+#Add User Rights Data
+
+#list of user rights lists
+#draw_List = []
+#
+col = 1
+rows = 1
+for rights_list in draw_List:
+    for record in rights_list:
+        sheet_Permissions.write(rows, col, record)
+        rows = rows + 1
+    col = col + 1
+    rows = 1
+
+#Save book
+
+#############################################################    
+book.save("C:\Users\mlemmon\Desktop\Michael\Permissions.xls")
 
